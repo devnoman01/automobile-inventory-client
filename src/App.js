8b4249e923cb635662e1b540e74144a886c61080
devnoman01 <devnoman01@gmail.com>
2022-05-09 17:03:59 +0600

    about page complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./Components/Header/Header";
+import About from "./Pages/About/About";
 import Login from "./Pages/Auth/Login/Login";
 import Register from "./Pages/Auth/Register/Register";
 import RequireAuth from "./Pages/Auth/RequireAuth/RequireAuth";
@@ -22,6 +23,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+
+        {/* authentication related route */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
